b3d07aab44c9577110631f733e3366d9566c8ec0
Anirudh Dayma <anirudh.daymaa@gmail.com>
2019-12-26 00:29:38 +0530

    File menu quit functionality pending

--- editor.py
+++ editor.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import font, colorchooser, messagebox, filedialog
 import tkinter.ttk as ttk
+import os
 
 # Creating a main app which will hold everything
 main_app = tk.Tk()
@@ -115,15 +116,15 @@ font_color_icon = tk.PhotoImage(file='icons/font_color.png')
 font_color_button = tk.Button(tool_bar, image=font_color_icon)
 font_color_button.grid(row=0, column=5, padx=5)
 
-# Align Center button
-align_center_icon = tk.PhotoImage(file='icons/align_center.png')
-align_center_button = tk.Button(tool_bar, image=align_center_icon)
-align_center_button.grid(row=0, column=6, padx=5)
-
 # Align Left button
 align_left_icon = tk.PhotoImage(file='icons/align_left.png')
 align_left_button = tk.Button(tool_bar, image=align_left_icon)
-align_left_button.grid(row=0, column=7, padx=5)
+align_left_button.grid(row=0, column=6, padx=5)
+
+# Align Center button
+align_center_icon = tk.PhotoImage(file='icons/align_center.png')
+align_center_button = tk.Button(tool_bar, image=align_center_icon)
+align_center_button.grid(row=0, column=7, padx=5)
 
 # Align Right button
 align_right_icon = tk.PhotoImage(file='icons/align_right.png')
@@ -146,7 +147,7 @@ text_editor.focus_set()
 # Expand text editor in X and Y direction(BOTH), expand = True option tells the manager to assign additional space to the widget box if parent widget is made larger.
 text_editor.pack(fill=tk.BOTH, expand=True)
 
-###------- Change font family and size
+##------- Change font family and size
 current_font_family = "Arial"
 current_font_size = 24
 
@@ -172,7 +173,23 @@ def change_font(event=None):
 font_family_box.bind('<<ComboboxSelected>>', change_font)
 font_size_box.bind('<<ComboboxSelected>>', change_font)
 
+# text_editor.config(font=(current_font_family, current_font_size))
+
+# def change_font(event=None):
+# 	# global current_font_family
+# 	global current_font_size
+
+# 	current_font_family = font_family_choice.get()
+# 	# current_font_size = font_size_choice.get()
+# 	new_font = tk.font.Font(font=text_editor["font"])
+# 	new_font.configure(size=current_font_size, family=current_font_family)
+# 	text_editor.configure(font=new_font)
+
+# font_family_box.bind("<<ComboboxSelected>>", change_font)
+# font_size_box.bind("<<ComboboxSelected>>", change_font_size)
+
 text_editor.config(font=(current_font_family, current_font_size))
+
 
 #------- Buttons functionality
 
@@ -226,23 +243,166 @@ def change_to_italic():
 		text_editor.configure(font=new_font)
 
 italic_button.configure(command=change_to_italic)
+
+### Change font color
+color_choice = None
+def change_font_color():
+	'''
+	A function to change font color by creating a tag and adding tag to text editor
+	'''
+	global color_choice
+	color_choice = tk.colorchooser.askcolor()
+	# tk.colorchooser.askcolor() creates a color chooser and returns the color chosen by user
+	# returns tuple: ((99.38671875, 225.87890625, 96.375), '#63e160')
+	# 1st value of tuple contains a tuple with R, R, B value and second is color code in hexa, so
+	# we need color code in hexa therefore color_choice[1] i.e 2nd value of tuple = #63e160
+	print(color_choice)
+	# text_editor.config(fg=color_choice[1]) 
+	# OR below 2 lines can be used
+	text_editor.tag_config('fg', foreground=color_choice[1]) #configure a tag name with a functionality
+	text_editor.tag_add('fg', 1.0, tk.END) #add that tag to text_editor
+	# Parameters of tag_add: tag name, start index, end index
+
+font_color_button.configure(command=change_font_color)
+
+### Alignment of text functionality
+
+# Align left
+def align_left():
+	'''
+	A function that aligns text to the left
+	1. copy content of text_editor to a variable
+	2. configure a tag with left alignment and foreground color
+	3. delete the content of text_editor
+	4. insert the copied conyent back to text editor with the tag
+	'''
+	# The below commented code gives weird behavior - look into it
+	# text_editor.tag_config('align_left', justify='left')
+	# text_editor.tag_add('align_left', 1.0, tk.END)
+	global color_choice
+	text_content = text_editor.get(1.0, tk.END) #1.0 indicates line 1 character 0
+	# The tag should also include foreground color or else color of text would change when alignment is changed
+	text_editor.tag_config('align_left', justify=tk.LEFT, foreground=color_choice[1])
+	text_editor.delete(1.0, tk.END)
+	# text_editor.tag_add('align_left', 1.0, tk.END)
+	text_editor.insert(tk.INSERT, text_content, 'align_left')
+
+align_left_button.configure(command=align_left)
+
+# Align center
+def align_center():
+	# text_editor.tag_config('align_center', justify='center')
+	# text_editor.tag_add('align_center', 1.0, tk.END)
+	global color_choice
+	text_content = text_editor.get(1.0, tk.END)
+	text_editor.tag_config('align_center', justify=tk.CENTER, foreground=color_choice[1])
+	text_editor.delete(1.0, tk.END)
+	# text_editor.tag_add('align_center', 1.0, tk.END)
+	text_editor.insert(tk.INSERT, text_content, 'align_center')
+	# text_editor.configure(font=new_font)
+
+align_center_button.configure(command=align_center)
+
+# Align right
+def align_right():
+	global color_choice
+	text_content = text_editor.get(1.0, tk.END)
+	new_font = tk.font.Font(font=text_editor["font"])
+	text_editor.tag_config('align_right', justify=tk.RIGHT, foreground=color_choice[1])
+	text_editor.delete(1.0, tk.END)
+	# text_editor.tag_add('align_right', 1.0, tk.END)
+	text_editor.configure(font=new_font)
+	text_editor.insert(tk.INSERT, text_content, 'align_right')
+
+align_right_button.configure(command=align_right)
+
+
+
 ############### ----------- END TEXT EDITOR ------------ ################
 
 ############################## STATUS BAR ###############################
-status_bar = tk.Label(main_app, text="Status bar")
+status_bar = tk.Label(text_editor, text="Status bar")
 status_bar.pack(side=tk.BOTTOM, fill=tk.X)
+
+def status_bar_update(event=None):
+	'''
+	A function to update satus bar with number of words and characters
+	'''
+	if text_editor.edit_modified():
+		# end-1c to skip the last trailing new line character that is added by default
+		words = len(text_editor.get(1.0, 'end-1c').split()) #split line into words
+		charcters = len(text_editor.get(1.0, 'end-1c').replace(' ','')) #Spaces won't be counted
+		status_bar.configure(text=f'Charcters: {charcters} Words: {words}')
+	text_editor.edit_modified(False) #if text is not modified set modified as false
+	
+text_editor.bind('<<Modified>>', status_bar_update)	#call function when event modified is triggered 
+
+
 ############### ----------- END STATUS BAR ------------ ################
 
 ############################## MAIN MENU FUNCTIONALITY ###############################
 
 # File menu commands
 # adding commands to the file menu
-file_menu.add_command(label="New", image=new_icon, compound=tk.LEFT, accelerator="Ctrl+N")
+
+### New file functionality
+url = '' #URL(path) of the file
+def new_file(event=None):
+	global url
+	text_editor.delete(1.0, tk.END)
+	main_app.title('New file')
+
+main_app.bind("<Control-n>", new_file)
+file_menu.add_command(label="New", image=new_icon, compound=tk.LEFT, accelerator="Ctrl+N", command=new_file)
 # compound = tk.LEFT aligns the iamge to left and label to right or else only image is shown
 # accelerator = "Ctrl+N" will show what key combination to be used as shortcut
-file_menu.add_command(label="open", image=open_icon, compound=tk.LEFT, accelerator="Ctrl+O")
-file_menu.add_command(label="Save", image=save_icon, compound=tk.LEFT, accelerator="Ctrl+S")
-file_menu.add_command(label="Save as", image=save_as_icon, compound=tk.LEFT, accelerator="Ctrl+Alt+S")
+
+### Open file functionality
+def open_file(event=None):
+	global url
+	url = filedialog.askopenfilename(initialdir=os.getcwd(), title='Select file', filetypes=(('Text file', '*.txt'), ('All files', '*.*')))
+	try:
+		with open(url, 'r') as f:
+			text_editor.delete(1.0, tk.END)
+			text_editor.insert(1.0, f.read())
+	except FileNotFoundError:
+		return
+	except:
+		return
+
+main_app.bind("<Control-o>", open_file)
+file_menu.add_command(label="open", image=open_icon, compound=tk.LEFT, accelerator="Ctrl+O", command=open_file)
+
+### Save file functionality
+def save_file(event=None):
+	global url
+	content = str(text_editor.get(1.0, tk.END))
+	try:
+		if url:
+			with open(url, 'w', encoding='utf-8') as f:
+				f.write(content)
+		else:
+			url = filedialog.asksaveasfilename(defaultextension='.txt', filetypes=(('Text file', '*.txt'), ('All files', '*.*')))
+			url.write(content)
+			url.close()
+	except:
+		return
+main_app.bind("<Control-s>", save_file)
+file_menu.add_command(label="Save", image=save_icon, compound=tk.LEFT, accelerator="Ctrl+S", command=save_file)
+
+### Save as functionlity
+def save_as(event=None):
+	global url
+	content = str(text_editor.get(1.0, tk.END))
+	try:
+		url = filedialog.asksaveasfilename(defaultextension='.txt', filetypes=(('Text file', '*.txt'), ('All files', '*.*')))
+		url.write(content())
+		url.close()
+	except:
+		return
+
+main_app.bind("<Alt-s>", save_as)
+file_menu.add_command(label="Save as", image=save_as_icon, compound=tk.LEFT, accelerator="Alt+S", command=save_as)
 file_menu.add_command(label="Exit", image=exit_icon, compound=tk.LEFT, accelerator="Ctrl+Q")
 
 # Edit menu commands
